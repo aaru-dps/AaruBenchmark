c09bcc673ef1b1439276586b5e04c2c14b458f16
Natalia Portillo <claunia@claunia.com>
2021-10-13 05:14:46 +0100

    Update file headers.

--- Aaru6.Checksums/CRC64/clmul.cs
+++ Aaru6.Checksums/CRC64/clmul.cs
@@ -9,23 +9,13 @@
 //
 // --[ Description ] ----------------------------------------------------------
 //
-// Compute the CRC32 using a parallelized folding approach with the PCLMULQDQ
+// Compute the CRC64 using a parallelized folding approach with the PCLMULQDQ
 // instruction.
 //
 // --[ License ] --------------------------------------------------------------
 //
-//     This library is free software; you can redistribute it and/or modify
-//     it under the terms of the GNU Lesser General Public License as
-//     published by the Free Software Foundation; either version 2.1 of the
-//     License, or (at your option) any later version.
-//
-//     This library is distributed in the hope that it will be useful, but
-//     WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-//     Lesser General Public License for more details.
-//
-//     You should have received a copy of the GNU Lesser General Public
-//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//     This file is under the public domain:
+//     https://github.com/rawrunprotected/crc
 //
 // ----------------------------------------------------------------------------
 // Copyright © 2011-2021 Natalia Portillo
